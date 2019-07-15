ba563b75a587b68bc3b33e3d6f05fc43c7334657
ArthurPinheiro <arthurpinheirodelima@gmail.com>
2019-07-14 23:06:32 -0300

    Padrozinando erros e adicionando paginação.

---
+++ src/main/java/br/com/devdojo/error/ValidationErrorDetails.java
@@ -0,0 +1,79 @@
+package br.com.devdojo.error;
+
+public class ValidationErrorDetails extends ErrorDetail {
+
+    private String field;
+    private String fieldMessage;
+
+    public String getField() {
+        return field;
+    }
+
+    public String getFieldMessage() {
+        return fieldMessage;
+    }
+
+    public static final class Builder {
+        private String title;
+        private int status;
+        private String detail;
+        private long timeStamp;
+        private String developMessage;
+        private String field;
+        private String fieldMessage;
+
+        private Builder() {
+        }
+
+        public static Builder newBuilder() {
+            return new Builder();
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder status(int status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder detail(String detail) {
+            this.detail = detail;
+            return this;
+        }
+
+        public Builder timeStamp(long timeStamp) {
+            this.timeStamp = timeStamp;
+            return this;
+        }
+
+        public Builder developMessage(String developMessage) {
+            this.developMessage = developMessage;
+            return this;
+        }
+
+        public Builder field(String field) {
+            this.field = field;
+            return this;
+        }
+
+        public Builder fieldMessage(String fieldMessage) {
+            this.fieldMessage = fieldMessage;
+            return this;
+        }
+
+        public ValidationErrorDetails build() {
+            ValidationErrorDetails validationErrorDetails = new ValidationErrorDetails();
+            validationErrorDetails.setTimeStamp(timeStamp);
+            validationErrorDetails.setTitle(title);
+            validationErrorDetails.setStatus(status);
+            validationErrorDetails.setDetail(detail);
+            validationErrorDetails.setDevelopMessage(developMessage);
+            validationErrorDetails.field = field;
+            validationErrorDetails.fieldMessage = fieldMessage;
+            return validationErrorDetails;
+        }
+    }
+}
--- src/main/java/br/com/devdojo/model/Student.java
+++ src/main/java/br/com/devdojo/model/Student.java
@@ -1,12 +1,18 @@
 package br.com.devdojo.model;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
 import javax.persistence.Entity;
 import java.util.Objects;
 
 @Entity
 public class Student extends AbstractEntity{
-
+    @NotEmpty
     private String name;
+    @NotEmpty
+    @Email
+    private String email;
 
     public String getName() {
         return name;
@@ -14,6 +20,14 @@ public class Student extends AbstractEntity{
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
